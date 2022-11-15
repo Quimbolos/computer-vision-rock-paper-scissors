ce5d2ad16b86a2239fb68484c2907c1e679c2228
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-15 15:32:48 +0000

    Task 2 - Milestone 5

--- camera_rps.py
+++ camera_rps.py
@@ -1,7 +1,58 @@
 # %% 
+import cv2
+from keras.models import load_model
+import numpy as np
+import random
+import time
+
+def get_computer_choice():
+
+    computer_choice = random.choice(["Rock","Paper","Scissors"])
+
+    return computer_choice
+
+def get_prediction():
+
+    model = load_model('keras_model.h5')
+    cap = cv2.VideoCapture(0)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    while True:
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        labels = ["Rock","Paper","Scissors","Nothing"]
+        prediction = model.predict(data)
+        prediction2 = labels[np.argmax(prediction)]
+        user_choice = prediction2
+        # cv2.imshow('frame', frame)
+        print(prediction2)
+        # Press q to close the window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    return user_choice
+
+def get_winner(user_choice = get_prediction(),computer_choice = get_computer_choice()):
+
+    if user_choice == computer_choice:
+        print("It is a tie!")
+
+    else:
+        if user_choice == 'Rock' and computer_choice == 'Scissors':
+            print("You won!")
+        elif user_choice == 'Scissors' and computer_choice == 'Paper':
+            print("You won!")
+        elif user_choice == 'Paper' and computer_choice == 'Rock':
+            print("You won!")
+        else:
+            print("You lost")
 
 
-# def get_prediction():
+get_winner()
 
-#     return
+# %%
+
 # %%
