fc3ad4da1b03a010bd2e71999daf94af8182977b
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-15 10:01:18 +0000

    Task 2 - Milestone 4

--- manual_rps.py
+++ manual_rps.py
@@ -1,3 +1,5 @@
+# %%
+
 import random
 
 def get_computer_choice():
@@ -11,3 +13,23 @@ def get_user_choice():
     user_choice = input("Enter your choice: ")
 
     return user_choice
+
+def get_winner(user_choice = get_user_choice(),computer_choice = get_computer_choice()):
+
+    if user_choice == computer_choice:
+        print("It is a tie!")
+
+    else:
+        if user_choice == 'Rock' & computer_choice == 'Scissors':
+            print("You won!")
+        elif user_choice == 'Scissors' & computer_choice == 'Paper':
+            print("You won!")
+        elif user_choice == 'Paper' & computer_choice == 'Rock':
+            print("You won!")
+        else:
+            print("You lost")
+
+    print(user_choice,computer_choice)
+
+get_winner()
+# %%
