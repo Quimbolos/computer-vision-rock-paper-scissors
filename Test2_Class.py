e3f8e220e718c8f1cccf250c2bd9d6a19197a956
Joaquim Bolós Fernández <joaquimbolosfernandez@Joaquims-MacBook-Pro.local>
2022-11-21 21:20:22 +0000

    Task 4 - Milestone 5

--- Test2_Class.py
+++ Test2_Class.py
@@ -14,11 +14,14 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 class RockPaperScissor():
 
-    def __init__ (self):
+    def __init__(self):
+        self.model = load_model('keras_model.h5', compile=False)
+        self.cap = cv2.VideoCapture(0)
+        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        self.labels = ["Rock","Paper","Scissors","Nothing"]
         self.computer_choice = self.get_computer_choice()
         self.user_choice = self.get_prediction()
-        self.model = load_model('keras_model.h5')
-        self.labels = ["Rock","Paper","Scissors","Nothing"]
+        
 
     def get_computer_choice(self):
 
@@ -26,17 +29,17 @@ class RockPaperScissor():
 
         return computer_choice
 
-    def get_prediction(self.model):
+    def get_prediction(self):
         
-        cap = cv2.VideoCapture(0)
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-
         while True:
+
+            ret, frame = self.cap.read()
+            if ret==True:
+                resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+                image_np = np.array(resized_frame)
+                normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+                self.data[0] = normalized_image
+                cv2.imshow('frame', frame)
 
             start = time.time()
             time_limit = 3 # Seconds
@@ -51,20 +54,24 @@ class RockPaperScissor():
 
             else:
 
-
-                prediction = model.predict(data, verbose = 0 )
+                prediction = self.model.predict(self.data, verbose = 0 )
                 prediction2 = self.labels[np.argmax(prediction)]
                 user_choice = prediction2
-                cv2.imshow('frame', frame)
                 print(prediction2)
                 break
 
         return user_choice
 
+    
+
     def get_winner(self):
     
         computer_wins = 0
         user_wins = 0
+
+        ret, frame = self.cap.read()
+
+        cv2.imshow('frame', frame)
 
         while True:
 
@@ -72,33 +79,33 @@ class RockPaperScissor():
 
             user_choice = self.get_prediction()
 
+
             if user_choice == "Nothing":
                 print("Try again!")
 
             elif user_choice == computer_choice:
                 print("It is a tie!")
 
+            elif user_choice == 'Rock' and computer_choice == 'Scissors':
+                print("User won 1 point!")
+                user_wins = user_wins + 1
+                print("User wins:", user_wins ," / Computer wins:", computer_wins)
+
+            elif user_choice == 'Scissors' and computer_choice == 'Paper':
+                print("User won 1 point!")
+                user_wins = user_wins + 1
+                print("User wins:", user_wins ," / Computer wins:", computer_wins)
+
+            elif user_choice == 'Paper' and computer_choice == 'Rock':
+                print("User won 1 point!")
+                user_wins = user_wins + 1
+                print("User wins:", user_wins ," / Computer wins:", computer_wins)
+
             else:
-                if user_choice == 'Rock' and computer_choice == 'Scissors':
-                    print("User won 1 point!")
-                    user_wins = user_wins + 1
-                    print("User wins:", user_wins ," / Computer wins:", computer_wins)
-
-                elif user_choice == 'Scissors' and computer_choice == 'Paper':
-                    print("User won 1 point!")
-                    user_wins = user_wins + 1
-                    print("User wins:", user_wins ," / Computer wins:", computer_wins)
-
-                elif user_choice == 'Paper' and computer_choice == 'Rock':
-                    print("User won 1 point!")
-                    user_wins = user_wins + 1
-                    print("User wins:", user_wins ," / Computer wins:", computer_wins)
-
-                else:
-                    print("Computer won 1 point!")
-                    computer_wins = computer_wins + 1
-                    print("User wins:", user_wins ," / Computer wins:", computer_wins)
-            
+                print("Computer won 1 point!")
+                computer_wins = computer_wins + 1
+                print("User wins:", user_wins ," / Computer wins:", computer_wins)
+        
             if user_wins == 3 or computer_wins == 3:
                 break
 
@@ -108,10 +115,18 @@ class RockPaperScissor():
         else: 
             print("You lost!")
 
+        self.cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
 
-            
 
-RockPaperScissor().get_winner()
+        
+
+# RockPaperScissor().get_winner()
+
+game = RockPaperScissor()
+
+game.get_winner()
 
 
 # %%
